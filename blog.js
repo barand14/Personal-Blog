cb8364a00f38b0c8334226679fb260369cec79b8
Balalomba Barandao <barand14@gmail.com>
2024-03-28 13:51:11 -0400

    created blog.js, form.js and logic.js

---
+++ blog.js
@@ -0,0 +1,22 @@
+#blog-form input,
+#blog-form textarea {
+    width: 100%;
+    padding: 0.5em;
+}
+
+#blog-form button {
+    display: block;
+    margin-top: 1em;
+    padding: 0.5em;
+
+}
+
+#blog-form label {
+    display: block;
+    margin-top: 1em;
+}
+
+.blog-title {
+    width: 50%;
+    margin: 0 auto;
+}    
